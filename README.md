0e730de4273f7845e4c8a2095f7e7aa6ab9ed62f
Horea Crisan <h22ea@icloud.com>
2022-06-07 21:12:05 +0300

    Liftoff! 🚀

---
+++ README.md
@@ -0,0 +1,5 @@
+##Zen Timer - Simple App for watchOS
+
+This app was created using SwiftUI.
+
+Understanding this codebase is great way of getting started using SwiftUI and creating watchOS apps. Using a simple and intuitive concept that allows you to get used to the very minimum watchOS development.
--- ZenTimer WatchKit Extension/ContentView.swift
+++ ZenTimer WatchKit Extension/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var secondScreenShown = false
+    @State var timerValue = 1
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("⏰ Start timer for \(timerValue) seconds")
+                .multilineTextAlignment(.center)
+            Picker(selection: $timerValue, label: Text("")) {
+                Text("1").tag(1)
+                Text("3").tag(3)
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+            }
+            NavigationLink(destination: SecondView(secondScreenShown: $secondScreenShown, timerValue: timerValue),
+                           isActive: $secondScreenShown,
+                           label: {Text("Go")})
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct SecondView: View {
+    @Binding var secondScreenShown: Bool
+    @State var timerValue: Int
+    
+    var body: some View {
+        VStack {
+            if timerValue > 0 {
+                Text("Time remaining")
+                    .font(.system(size: 14))
+                Text("\(timerValue)")
+                    .font(.system(size: 40))
+                    .onAppear(){
+                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                            if self.timerValue > 0 {
+                                self.timerValue -= 1
+                            }
+                        }
+                    }
+                Text("seconds")
+                    .font(.system(size: 14))
+                Button(action: {
+                    self.secondScreenShown = false // Unwind the navigation
+                }) {
+                    Text("Cancel")
+                        .foregroundColor(.red)
+                }
+            } else {
+                Button(action: {
+                    self.secondScreenShown = false
+                }) {
+                    Text("Done")
+                        .foregroundColor(.green)
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
